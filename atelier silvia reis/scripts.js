4b71b77d8149f648ec2a7c2ff6db2c543611303d
pdirafa <153040843+pdirafa@users.noreply.github.com>
2024-04-12 23:33:07 -0300

    arquivos do site Atelier Silvia Reis
    arquivos e imagens necessárias para o funcionamento do site

---
+++ atelier silvia reis/scripts.js
@@ -0,0 +1,52 @@
+// Scripts para Silvia Reis Atelier
+// Adicione seus scripts personalizados aqui
+
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode'); // Adiciona ou remove a classe dark-mode do body
+}
+
+const modoEscuroBotao = document.querySelector('.modo-escuro button');
+if (modoEscuroBotao) {
+    modoEscuroBotao.addEventListener('click', toggleDarkMode);
+}
+
+// Função para inicializar o mapa
+function inicializarMapa() {
+    // Cria um novo mapa
+    var mapa = L.map('mapa').setView([-19.628613, -43.225014], 18);
+
+    // Adiciona o mapa base (OpenStreetMap)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+    // Adiciona um marcador ao mapa 
+    var marcador = L.marker([-19.628613, -43.225014]).addTo(mapa);
+    marcador.bindPopup("<b>Silvia Reis Atelier</b><br>Av. Duque de Caxias, 1199.").openPopup();
+}
+
+// Chama a função de inicialização do mapa quando o documento estiver pronto
+document.addEventListener('DOMContentLoaded', inicializarMapa);
+
+// Abre o modal com a imagem clicada
+function openModal(img) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Fecha o modal quando o botão de fechar é clicado
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+// Fecha o modal quando o usuário clica fora da imagem
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
